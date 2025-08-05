aba1b9307d25f2f4ca9167bfc878bffc3391c6c9
Lucy <jinh2376@gmail.com>
2025-08-05 16:05:33 +0900

    baseURL 수정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { getAccessToken } from "../store/tokenManager";
 
-// const baseURL = process.env.REACT_APP_API_URL;
-const baseURL = "http://localhost:8001";
+const baseURL = process.env.REACT_APP_API_URL;
 
 // Instance for public requests (e.g., login, signup)
 export const publicApi = axios.create({
