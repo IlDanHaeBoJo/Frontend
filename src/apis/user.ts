0408d5c05236a7c2cba23be886c44d75819ccbf1
Lucy <jinh2376@gmail.com>
2025-08-13 10:45:05 +0900

    비밀번호 변경 api 연결

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,6 +1,12 @@
 import { api } from "./index";
+import { ChangePasswordRequest } from "../types/user";
 
 export const getStudents = async () => {
   const response = await api.get("/users/students");
+  return response.data;
+};
+
+export const changePassword = async (data: ChangePasswordRequest) => {
+  const response = await api.patch("/auth/change-password", data);
   return response.data;
 };
--- src/types/user.ts
+++ src/types/user.ts
@@ -7,3 +7,8 @@ export interface User {
   student_id?: string;
   major?: string;
 }
+
+export interface ChangePasswordRequest {
+  current_password: string;
+  new_password: string;
+}
